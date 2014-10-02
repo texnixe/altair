e37616d824f19170ab2d87d77a43402a68de973a
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-10-02 08:59:28 +0200

    Fix pagination to work with Kirby 2.

--- site/snippets/pagination.php
+++ site/snippets/pagination.php
@@ -11,46 +11,43 @@
 <div role="pagination" class="Pagination">
 
 	<p class="Pagination-count">
-		<strong><?php echo $pagination->countItems(); ?></strong> results / showing <strong><?php echo $pagination->numStart(); ?></strong> &ndash; <strong><?php echo ($pagination->countItems() < $pagination->numEnd()) ? $pagination->countItems() : $pagination->numEnd(); ?></strong>
+		<strong><?php echo $pagination->items(); ?></strong> results / showing <strong><?php echo $pagination->numStart(); ?></strong> &ndash; <strong><?php echo ($pagination->items() < $pagination->numEnd()) ? $pagination->items() : $pagination->numEnd(); ?></strong>
 	</p>
 
 	<?php if($pagination->countItems() > $pagination->limit()): ?>
 	<nav>
-		<?php if(isset($range) && $pagination->countPages() > $range): ?>
+		<h2 class="u-isHiddenVisually">Page navigation</h2>
+		<?php if(isset($range) && $pagination->items() > $range): ?>
 			<?php if(!$pagination->isFirstPage()): ?>
-				<a href="<?php echo (str::substr($pagination->firstPageURL(),0,-7)); ?>" class="Pagination-first">&larr;&larr; First</a>
+				<a href="<?php echo $pagination->firstPageURL(); ?>" class="Pagination-first">&larr;&larr; Newest</a>
 			<?php else: ?>
-				<span class="Pagination-first">&larr;&larr; First</span>
+				<span class="Pagination-newest">(&larr;&larr; Newest)</span>
 			<?php endif; ?>
 		<?php endif; ?>
 
 		<?php if($pagination->hasPrevPage()): ?>
-			<?php if($pagination->page() == 2): ?>
-				<a href="<?php echo (str::substr($pagination->firstPageURL(),0,-7)); ?>" class="Pagination-prev">&larr; Previous</a>
-			<?php else: ?>
-				<a href="<?php echo $pagination->prevPageURL(); ?>" class="Pagination-prev">&larr; Previous</a>
-			<?php endif; ?>
+			<a href="<?php echo $pagination->prevPageURL(); ?>" class="Pagination-newer">&larr; Newer</a>
 		<?php else: ?>
-			<span class="Pagination-prev">&larr; Previous</span>
+			<span class="Pagination-newer">(&larr; Newer)</span>
 		<?php endif; ?>
 
-		<?php if(isset($range) && $pagination->countPages() > 1): ?>
+		<?php if(isset($range) && $pagination->items() > 1): ?>
 			<?php foreach($pagination->range($range) as $range_num): ?>
-				<a href="<?php echo $pagination->pageURL($range_num) ?>" class="Pagination-range<?php if($pagination->page() == $range_num) echo ' is-active' ?>"><?php echo $range_num ?></a></li>
+				<a href="<?php echo $pagination->pageURL($range_num) ?>" class="Pagination-range<?php if($pagination->page() == $range_num) echo ' is-active' ?>"><?php echo $range_num ?></a>
 			<?php endforeach ?>
 		<?php endif; ?>
 
 		<?php if($pagination->hasNextPage()): ?>
-			<a href="<?php echo $pagination->nextPageURL(); ?>" class="Pagination-next">Next &rarr;</a>
+			<a href="<?php echo $pagination->nextPageURL(); ?>" class="Pagination-older">Older &rarr;</a>
 		<?php else: ?>
-			<span class="Pagination-next">Next &rarr;</span>
+			<span class="Pagination-older">(Older &rarr;)</span>
 		<?php endif; ?>
 
-		<?php if(isset($range) && $pagination->countPages() > $range): ?>
+		<?php if(isset($range) && $pagination->items() > $range): ?>
 			<?php if(!$pagination->isLastPage()): ?>
-				<a href="<?php echo $pagination->lastPageURL() ?>" class="Pagination-last">Last &rarr;&rarr;</a>
+				<a href="<?php echo $pagination->lastPageURL() ?>" class="Pagination-oldest">Oldest &rarr;&rarr;</a>
 			<?php else: ?>
-				<span class="Pagination-last">Last &rarr;&rarr;</span>
+				<span class="Pagination-oldest">(Oldest &rarr;&rarr;)</span>
 			<?php endif; ?>
 		<?php endif; ?>
 	</nav>
