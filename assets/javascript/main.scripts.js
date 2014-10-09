9e278948e3fc63c688dc15904cc9cce647976dc7
Marijn Tijhuis <marijn@studiodumbar.com>
2014-10-09 14:48:36 +0200

    Invoke javascript on window load.
    Fixes bug with addEventListener not finding DOM objects.

--- assets/javascript/main.scripts.js
+++ assets/javascript/main.scripts.js
@@ -7,15 +7,13 @@
 // Only serve javascript to 'Cutting the Mustard' browsers
 if(enhance.ctm()){
 
-	(function (window, document) {
-
+	window.addEventListener('load', function() {
 		/* Initiate all available classes */
 		alerts.init(push_message);                // Init alerts
 		expand.init();                            // Init expand / collapse
 		navMain.init();                           // Init main navigation
 		popup.init();                             // Init popup
 		lazyload.init();                     // Init lazyload with resrc
-
-	})(window, document);
+	}, false);
 
 }
