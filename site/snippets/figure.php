5e396b59fb44040cf9632343fad6bb51e28e10a8
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-10-09 09:58:59 +0200

    Update figure snippet.
    Add option to pass a class, and add ‘FigureImage-item’ class to img tag by default.

--- site/snippets/figure.php
+++ site/snippets/figure.php
@@ -5,15 +5,15 @@
 // Example embed code:
 //
 // echo snippet('figure', array(
-// 		'image'  => $list_image_object,
-// 		'alt' => 'Alt text',
-// 		'class' => 'ImageClassWhoah'
+// 		'image'  => $image,
+// 		'class'  => $class,
+// 		'alt'    => smartypants($page->title()),
 // ));
 // ----------------------------------------------------------
 
 // Only for development env's, set a smaller thumb instead of full size
 if(c::get('environment') == 'local' && c::get('resrc') == false) {
-	$width = c::get('thumb.dev.width',1200);
+	$width = c::get('thumb.dev.width',800);
 }
 
 $thumburl = thumb($image,array(
@@ -23,25 +23,22 @@ $thumburl = thumb($image,array(
 
 ////////////////////////////////////////////////////////// ?>
 
-<figure>
+<figure class="FigureImage">
 
-<?php if(c::get('lazyload') == false && c::get('resrc') == false) : // Regular image tag, full size thumbnail ?>
-	<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="<?php echo $class; ?>" alt="<?php echo html($alt); ?>" />
-<?php endif; ?>
+	<?php if(c::get('lazyload') == false && c::get('resrc') == false) : // Regular image tag, full size thumbnail ?>
+		<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item <?php echo $class; ?>" alt="<?php echo $alt; ?>" />
+	<?php endif; ?>
 
+	<?php if(c::get('lazyload') == true && c::get('resrc') == false) : // Lazyload (echo.js) image tag ?>
+		<img data-src="<?php echo $thumburl; ?>" src="/assets/images/loader.gif" class="FigureImage-item <?php echo $class; ?>" alt="<?php echo $alt; ?>" />
+	<?php endif; ?>
 
-<?php if(c::get('lazyload') == true && c::get('resrc') == false) : // Lazyload (echo.js) image tag ?>
-	<img data-src="<?php echo $thumburl; ?>" src="/assets/images/loader.gif" width="" height="" class="" alt="<?php echo html($alt); ?>" />
-<?php endif; ?>
+	<?php if(c::get('lazyload') == false && c::get('resrc') == true) : // Resrc image tag ?>
+		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item <?php echo $class . ' resrc'; ?>" alt="<?php echo $alt; ?>" />
+	<?php endif; ?>
 
-
-<?php if(c::get('lazyload') == false && c::get('resrc') == true) : // Resrc image tag ?>
-	<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="<?php echo $class . ' resrc'; ?>" alt="<?php echo html($alt); ?>" />
-<?php endif; ?>
-
-
-<?php if(c::get('lazyload') == true && c::get('resrc') == true) : // Resrc image via lazyload (echo.js) image tag ?>
-	<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" src="/assets/images/loader.gif" width="" height="" class="<?php echo $class . ' js-resrcIsLazy'; ?>" alt="<?php echo html($alt); ?>" />
-<?php endif; ?>
+	<?php if(c::get('lazyload') == true && c::get('resrc') == true) : // Resrc image via lazyload (echo.js) image tag ?>
+		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" src="/assets/images/loader.gif" class="FigureImage-item  <?php echo $class . ' js-resrcIsLazy'; ?>" alt="<?php echo $alt; ?>" />
+	<?php endif; ?>
 
 </figure>
