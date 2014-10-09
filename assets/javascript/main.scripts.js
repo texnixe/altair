c0ed94eacc67daa4f79ae2cd57f772aed8aa4206
Marijn Tijhuis <marijn@studiodumbar.com>
2014-10-09 16:18:59 +0200

    Revert script file to standard state.

--- assets/javascript/main.scripts.js
+++ assets/javascript/main.scripts.js
@@ -7,13 +7,13 @@
 // Only serve javascript to 'Cutting the Mustard' browsers
 if(enhance.ctm()){
 
-	window.addEventListener('load', function() {
+	(function (window, document) {
 		/* Initiate all available classes */
 		alerts.init(push_message);                // Init alerts
 		expand.init();                            // Init expand / collapse
 		navMain.init();                           // Init main navigation
 		popup.init();                             // Init popup
 		lazyload.init();                     // Init lazyload with resrc
-	}, false);
+	})(window, document);
 
 }
