efde79509e945437200e21d6d1198b1064f6cfe5
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-10-09 16:42:28 +0200

    Update snippet figure.
    Check for undefined variabels.

--- site/snippets/figure.php
+++ site/snippets/figure.php
@@ -17,8 +17,7 @@ if(c::get('environment') == 'local' && c::get('resrc') == false) {
 }
 
 $thumburl = thumb($image,array(
-	'width' => $width, // use maximum image width for thumb
-	'height' => $height
+	'width' => $width, // use maximum image width for thumb; height follows width ratio
 ), false);
 
 ////////////////////////////////////////////////////////// ?>
@@ -26,19 +25,19 @@ $thumburl = thumb($image,array(
 <figure class="FigureImage">
 
 	<?php if(c::get('lazyload') == false && c::get('resrc') == false) : // Regular image tag, full size thumbnail ?>
-		<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item <?php echo $class; ?>" alt="<?php echo $alt; ?>" />
+		<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item<?php if(isset($class)): echo ' ' . $class; endif; ?>" alt="<?php ecco(isset($alt), $alt); ?>" />
 	<?php endif; ?>
 
 	<?php if(c::get('lazyload') == true && c::get('resrc') == false) : // Lazyload (echo.js) image tag ?>
-		<img data-src="<?php echo $thumburl; ?>" src="/assets/images/loader.gif" class="FigureImage-item <?php echo $class; ?>" alt="<?php echo $alt; ?>" />
+		<img data-src="<?php echo $thumburl; ?>" src="/assets/images/loader.gif" class="FigureImage-item<?php if(isset($class)): echo ' ' . $class; endif; ?>" alt="<?php if(isset($alt)): echo $alt; endif; ?>" />
 	<?php endif; ?>
 
 	<?php if(c::get('lazyload') == false && c::get('resrc') == true) : // Resrc image tag ?>
-		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item <?php echo $class . ' resrc'; ?>" alt="<?php echo $alt; ?>" />
+		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item resrc<?php if(isset($class)): echo ' ' . $class; endif; ?>" alt="<?php ecco(isset($alt), $alt); ?>" />
 	<?php endif; ?>
 
 	<?php if(c::get('lazyload') == true && c::get('resrc') == true) : // Resrc image via lazyload (echo.js) image tag ?>
-		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" src="/assets/images/loader.gif" class="FigureImage-item  <?php echo $class . ' js-resrcIsLazy'; ?>" alt="<?php echo $alt; ?>" />
+		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" src="/assets/images/loader.gif" class="FigureImage-item js-resrcIsLazy<?php if(isset($class)): echo ' ' . $class; endif; ?>" alt="<?php ecco(isset($alt), $alt); ?>" />
 	<?php endif; ?>
 
 </figure>
